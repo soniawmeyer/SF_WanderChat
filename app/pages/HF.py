4cd1b909483b35292b67607f59be618dbc7792b3
chriztopherton <christopher.ton@sjsu.edu>
2024-05-04 11:01:36 -0700

    rename files

---
+++ app/pages/HF.py
@@ -0,0 +1,56 @@
+import streamlit as st 
+import time 
+import random 
+from openai import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+st.set_page_config(page_title="WanderChat", page_icon=":speech_balloon:")
+st.sidebar.title("Mistral RAFT")
+
+hf_token = os.getenv("hf_token")
+
+with st.sidebar:
+    with st.expander("HF token"):
+        user_hf_token = st.text_input("","",key="hf_key")
+    with st.expander("LLM Inference Endpoint"):
+        model_base_url = st.text_input("","",key="model_key")
+    
+if user_hf_token and model_base_url:
+    chat = ChatOpenAI(
+        model_name="tgi",
+        openai_api_key=user_hf_token,
+        openai_api_base=model_base_url,
+    )
+    memoryforchat=ConversationBufferMemory()
+    convo=ConversationChain(memory=memoryforchat,llm=chat,verbose=True)
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history=[]
+    else:
+        for message in st.session_state.chat_history:
+            memoryforchat.save_context({"input":message["human"]},{"outputs":message["AI"]})
+
+    if "message" not in st.session_state:
+        st.session_state.message = [{"role":"assistant","content":"how may i help you "}]
+    for message1 in st.session_state.message:
+        with st.chat_message(message1["role"]):
+            st.markdown(message1["content"])
+            
+    if prompt:=st.chat_input("Say Something"):
+            with st.chat_message("user"):
+                st.markdown(prompt)
+                st.session_state.message.append({"role":"user","content":prompt})
+                
+            with st.chat_message("assistant"):
+                    with st.spinner("Thinking..."):
+                        time.sleep(1)
+                        responce=convo.predict(input=prompt)
+                        st.write(responce)
+            st.session_state.message.append({"role":"assistant","content":responce})
+            message={'human':prompt,"AI":responce}
+            st.session_state.chat_history.append(message)
