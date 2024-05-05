27622e77998a8c7059e836d207ac964ee26ddd77
chriztopherton <christopher.ton@sjsu.edu>
2024-05-04 20:12:55 -0700

    image links

--- app/pages/HF.py
+++ app/pages/HF.py
@@ -22,7 +22,7 @@ from utils.UI import *
 load_dotenv()
 
 st.set_page_config(page_title="WanderChat", page_icon=":speech_balloon:",layout="wide")
-add_logo("https://en.wikipedia.org/wiki/File:San_Jose_State_Spartans_logo.svg","WanderChat, a context-aware travel chatbot.")
+add_logo("./static/San_Jose_State_Spartans_logo.png","WanderChat, a context-aware travel chatbot.")
 
 # st.sidebar.title("Custom model")
 
